af5dec06d7cf727301148abd96f6f4ed347cfd77
devkarem <karemmahmed79@gmail.com>
2025-02-04 22:55:28 +0200

    added some styling

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,19 +1,23 @@
-import { Component, OnInit,Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { MealsService } from './../../meals.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent implements OnInit {
   categories: any[] = [];
-  constructor(private _MealsService: MealsService) { }
+  selectedCategory: string = 'all';
+
   @Output() categorySelected = new EventEmitter<string>();
+  constructor(private _MealsService: MealsService) { }
 
   onSelectCategory(category: string): void {
     this.categorySelected.emit(category);
+    this.selectedCategory = category;
   }
   ngOnInit(): void {
     this._MealsService.getCategories().subscribe({
@@ -25,5 +29,8 @@ export class NavbarComponent implements OnInit {
       }
     })
   }
-
+  status: boolean = false;
+  clickEvent(){
+      this.status = !this.status;
+  }
 }
